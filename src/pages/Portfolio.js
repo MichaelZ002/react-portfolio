01be3ce6f0d23a024d85f380d3a5c8e8a4c57c8c
Michael Zaslavsky <mikezasl@gmail.com>
2020-10-13 14:34:00 -0500

    added to Portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,12 +1,12 @@
 import React from "react";
 import MoVic from '../content/movic.PNG'
 import Dnd from '../content/dnd.PNG'
-
+import Fit from '../content/fit.PNG'
 function Portfolio(){
 
     return (
         <div className="container-fluid">
-        <h2>These are some of my projects!</h2>
+        <h2>Some Projects!</h2>
         <br />
         <div className="row">
             <div className="col-md-3">
@@ -24,6 +24,13 @@ function Portfolio(){
             <p>This is a Dungeons and Dragons character maker!</p>
                 <p>you can find the github here! <a href =" https://github.com/MichaelZ002/project-two-group-zero">here!</a></p>
                 <p>You can find the deploy app <a href ="https://peaceful-everglades-53655.herokuapp.com/">here! </a></p>
+                
+                <img src={Fit} alt="f" className="img img-fluid mx-auto d-block" />
+                <p>Work out tracker </p>
+                <p>you can find the github here! <a href =" https://github.com/MichaelZ002/homework_seventeen">here!</a></p>
+                <p>You can find the deploy app <a href ="https://serene-beach-10522.herokuapp.com/?id=5f5847d82089ca001738ff1b">here! </a></p>
+                
+                
             </div>
         </div>
     </div>
