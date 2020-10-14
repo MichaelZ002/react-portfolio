e910a594b7c9571ea1851bb43aa7fbf2fa6b756d
Michael Zaslavsky <mikezasl@gmail.com>
2020-10-14 02:07:09 -0500

    made nav bar dark

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,29 +1,27 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-
-
-function Nav(){
+function Nav() {
 
     return (
         <div id="header" className="container-fluid top">
-        <nav className="navbar navbar-light bg-light">
-            <Link className="navbar-brand" to="/">Michael Zaslavsky</Link>
-            <header className="navbar" id="navbarSupportedContent">
-                <ul className="navbar-nav list-group-horizontal header-links">
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/about">About</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/portfolio">Portfolio</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/contact">Contact</Link>
-                    </li>
-                </ul>
-            </header>
-        </nav>
-    </div>
+            <nav class="navbar navbar-dark bg-dark">
+                <Link className="navbar-brand" to="/">Michael Zaslavsky</Link>
+                <header className="navbar" id="navbarSupportedContent">
+                    <ul className="navbar-nav list-group-horizontal header-links">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about">About</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/portfolio">Portfolio</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/contact">Contact</Link>
+                        </li>
+                    </ul>
+                </header>
+            </nav>
+        </div>
     )
 }
 
